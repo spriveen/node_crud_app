e480d11865986bc16b4617e7b55b9864745696e9
spriveen <rpeanuka@gmail.com>
2024-08-12 04:10:33 -0700

    update add_user.ejs

--- routes/routes.js
+++ routes/routes.js
@@ -7,6 +7,11 @@ router.get('/', (req, res) => {
     res.render('index', { title: 'Home Page' });
 });
 
+router.get('/add',(req,res) =>{
+res.render('add_users', {title: "Add Users"});
+
+});
+
 // Make sure there are no extra or missing braces
 module.exports = router;
 
