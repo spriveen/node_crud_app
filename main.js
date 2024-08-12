a0d1e124de81d66ce0d3e7807dd6d1a8c5759afd
spriveen <rpeanuka@gmail.com>
2024-08-12 01:35:12 -0700

    update set template engine

--- main.js
+++ main.js
@@ -12,10 +12,31 @@ mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology
   .then(() => console.log('Database connected successfully'))
   .catch(err => console.log('Database connection error:', err));
 
-// middleware
+
+  // middleware
+app.use (express.urlencoded({extended:false}));
 app.use(express.json());
 
-  
+ app.use(
+    session({
+    secret: 'my secret key',
+    saveUninitialized: true,
+    resave: false
+
+})
+); 
+app.use ((req, res, next)=> {
+    res.locals.message = req.session.message
+    delete req.session.message;
+    next();
+})
+
+// set template engine
+app.set('view engine', 'ejs');
+
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
