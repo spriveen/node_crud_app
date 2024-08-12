6aa79a60337f4973472fc8fde95ec3ce4bb87e0a
spriveen <rpeanuka@gmail.com>
2024-08-12 03:32:13 -0700

    update navbar.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
+// Example of correct route
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (req,res) =>{
-  res.send("Home Page");
+// Your route definitions
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Home Page' });
 });
 
+// Make sure there are no extra or missing braces
 module.exports = router;
+
