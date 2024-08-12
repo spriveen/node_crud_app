fc2d5a8cc60f54a0f2d79a684e2a790a650c41db
spriveen <rpeanuka@gmail.com>
2024-08-12 01:01:35 -0700

    update data base

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
 // imports
-require('dotenv').config
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// database connections
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Database connected successfully'))
+  .catch(err => console.log('Database connection error:', err));
+
+// middleware
+app.use(express.json());
+
+  
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`)
+});
